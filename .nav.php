a54dd666206dedb80e847ce211a4c941453fb166
ADRIANVEGA9 <vegaadrian@gmail.com>
2013-10-09 09:36:40 -0500

    inicio
    plantilla base, header con logo y menú, nav horizontal, menu productos
    (tipo acordeón), footer estatico

---
+++ .nav.php
@@ -0,0 +1,12 @@
+<nav>
+	<table>
+		<tr>
+			<td><a href="#">nuevos productos</a></td>
+			<td class="activo" ><a class="activo" href="#">catálogos</a></td>
+			<td><a href="#">vídeos</a></td>
+			<td><a href="#">eventos</a></td>
+			<td class="cuenta"><div id="cuenta"><a href="#">mi cuenta</a></div><div class="triangulo_top_right"></div></td>
+			<td id="buscar"><a href="#"><input type="search" required></a></td>
+		</tr>
+	</table>
+</nav>
